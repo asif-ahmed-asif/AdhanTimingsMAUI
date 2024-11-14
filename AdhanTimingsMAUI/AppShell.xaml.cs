a4d61f119456d395aa6929a1b449c74e6bdf1a39
asif <asif@techknowworld.com>
2024-11-14 18:56:27 +0600

    Adhan library added

---
+++ AdhanTimingsMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AdhanTimingsMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AdhanTimingsMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AdhanTimingsMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
